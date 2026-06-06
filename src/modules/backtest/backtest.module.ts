7ced0a1db818554fb7d998aa7542d77e8ad35a19
vraj7 <vraj.areekat@gmail.com>
2026-06-06 11:13:33 +0530

    fix for local

--- src/modules/backtest/backtest.module.ts
+++ src/modules/backtest/backtest.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BacktestResultEntity } from '../../entities';
+import { DataModule } from '../data/data.module';
+import { IndicatorsModule } from '../indicators/indicators.module';
+import { SignalModule } from '../signal/signal.module';
 import { BacktestService } from './backtest.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BacktestResultEntity])],
+  imports: [
+    TypeOrmModule.forFeature([BacktestResultEntity]),
+    DataModule.register(),
+    IndicatorsModule,
+    SignalModule.register(),
+  ],
   providers: [BacktestService],
   exports: [BacktestService],
 })
